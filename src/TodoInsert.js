e7547e48c20cdbb4e794dc2234e22958834a546a
woo <choiwu0917@gmail.com>
2022-02-20 15:16:20 +0900

    finish

---
+++ src/TodoInsert.js
@@ -0,0 +1,27 @@
+import React,{useState,useCallback} from "react";
+import {MdAdd} from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert=({onInsert})=>{
+    const [value, setValue]=useState('');
+    const onChange=useCallback(event=>{
+        setValue(event.target.value);
+    },[]);
+    const onSubmit=useCallback(
+        event=>{
+            onInsert(value);
+            setValue('');
+            event.preventDefault();
+        },
+        [onInsert,value],
+    );
+    return (
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input placeholder="Write to do" value={value} onChange={onChange}/>
+            <button type="submit">
+                <MdAdd/>
+            </button>
+        </form>
+    );
+};
+export default TodoInsert;
